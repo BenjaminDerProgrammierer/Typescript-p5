64f34f8e10a30e50127806eb5d3589b9e843a28a
BenjaminDerProgrammierer <benjamin.p.hartmann@gmail.com>
2022-01-21 18:43:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-p5-103
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-p5-103)
---
+++ index.ts
@@ -0,0 +1,33 @@
+// Import stylesheets
+import './style.css';
+// Importiere p5
+import p5 = require('p5');
+
+// Funktion Setup
+function setup(p: p5) {
+  p.createCanvas(500, 450); // Mache Bereich
+}
+
+//Funktion Draw
+function draw(p: p5) {
+  p.background('black'); // Hintergrund Schwarz
+  rectangles(p); // rufe Funktion Rechtecke auf
+}
+
+//Rechtecke
+function rectangles(p: p5) {
+  p.colorMode(p.HSB, 500); //Farbmodus HSB
+  for (let x = 25; x < 475; x += 25) {
+    //Widerhohle
+    for (let y = 25; y < 425; y += 25) {
+      //Widerhohle
+      p.fill(x, 500, 500); //Fülle aus
+      p.rect(x + p.random(-2, 2), y + p.random(-2, 2), 20, 20); //Zufällige Rechtecke erstellen
+    }
+  }
+}
+const p = new p5((p: p5) => {
+  p.setup = () => setup(p);
+  p.draw = () => draw(p);
+  return p;
+});
